5917f5c51fded4cda1044c5b1ee42a1a359c7a54
Regis Antunes <65835931+regismantunes@users.noreply.github.com>
2025-07-12 16:44:23 -0300

    Implemented FolderSearcher

---
+++ RA.Utilities/Folder/FolderSearcher.cs
@@ -0,0 +1,129 @@
+﻿using RA.Utilities.Extensions;
+using RA.Utilities.Output;
+using RA.Utilities.Windows;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Security.AccessControl;
+using System.Security.Principal;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RA.Utilities.Folder
+{
+    public class FolderSearcher(FolderSearcherOptions options)
+    {
+        public FolderSearcher()
+            : this(new FolderSearcherOptions())
+        { }
+
+        public FolderSearcherOptions Options { get; private set; } = options ?? throw new ArgumentNullException(nameof(options));
+
+        public bool IsRunning { get; private set; } = false;
+        public string LastPath { get; private set; } = string.Empty;
+        private readonly object _locker = new();
+
+        public void Search(Action<string> onFind)
+        {
+            Search(Options, onFind);
+        }
+
+        public void Search(FolderSearcherOptions options, Action<string> onFind)
+        {
+            ArgumentNullException.ThrowIfNull(onFind, nameof(onFind));
+
+            lock (_locker)
+            {
+                if (IsRunning)
+                    throw new InvalidOperationException("Search is already running.");
+
+                IsRunning = true;
+            }
+
+            try
+            {
+                Options = options ?? throw new ArgumentNullException(nameof(options));
+                ExecuteSearch(onFind);
+            }
+            finally
+            {
+                IsRunning = false;
+            }
+        }
+
+        private void ExecuteSearch(Action<string> onFind)
+        { 
+            var currentUser = Identity.GetCurrentSID();
+            var countTask = 0;
+            var enumerationOptions = new EnumerationOptions
+            {
+                RecurseSubdirectories = false,
+                AttributesToSkip = Options.AttributesToSkip,
+                IgnoreInaccessible = true,
+                ReturnSpecialDirectories = Options.ReturnSpecialDirectories
+            };
+
+            void searchInFolder(string path)
+            {
+                if (Options.FindForFiles)
+                {
+                    try
+                    {
+                        var files = Directory.GetFiles(path, Options.FileSearchPattern, enumerationOptions);
+                        foreach (var file in files)
+                        {
+                            var fileInfo = new FileInfo(file);
+                            var rules = fileInfo.GetAccessControl()
+                                                .GetAccessRules(true, true, typeof(SecurityIdentifier));
+                            foreach (FileSystemAccessRule rule in rules)
+                            {
+                                if ((!Options.FileSystemRights.HasValue ||
+                                    (rule.IdentityReference.Value == currentUser && (rule.FileSystemRights & Options.FileSystemRights) == Options.FileSystemRights)) &&
+                                    rule.AccessControlType == AccessControlType.Allow)
+                                {
+                                    onFind(file);
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                    catch { }
+                }
+
+                searchInSubFolders(path);
+            }
+
+            void searchInSubFolders(string path)
+            {
+                try
+                {
+                    var folders = Directory.GetDirectories(path, Options.DirectorySearchPattern, enumerationOptions);
+                    foreach (var folder in folders)
+                    {
+                        if (folder.EndsWith("\\..") ||
+                            folder.EndsWith("\\."))
+                            continue;
+
+                        if (Options.FindForDirectories)
+                            onFind(folder);
+
+                        Interlocked.Increment(ref countTask);
+
+                        Task.Run(() =>
+                        {
+                            searchInFolder(folder);
+                            Interlocked.Decrement(ref countTask);
+                        });
+                    }
+                }
+                catch { }
+            }
+
+            searchInFolder(Options.InitialPath);
+
+            while (countTask > 0)
+                Thread.Sleep(100);
+        }
+    }
+}
---
+++ RA.Utilities/Folder/FolderSearcherOptions.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Security.AccessControl;
+
+namespace RA.Utilities.Folder
+{
+    public class FolderSearcherOptions
+    {
+        public string InitialPath { get; set; }
+
+        public int Taken { get; set; }
+
+        public FileSystemRights? FileSystemRights { get; set; }
+
+        public FileAttributes AttributesToSkip { get; set; } = FileAttributes.None;
+
+        public bool ReturnSpecialDirectories { get; set; } = true;
+
+        public string DirectorySearchPattern { get; set; } = "*";
+
+        public string FileSearchPattern { get; set; } = "*";
+
+        public bool FindForFiles { get; set; } = true;
+
+        public bool FindForDirectories { get; set; } = true;
+    }
+}
