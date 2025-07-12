92b5d2ccf3ad805aaff71e1d87de6460beddceed
Regis Antunes <65835931+regismantunes@users.noreply.github.com>
2025-07-12 16:44:38 -0300

    Implemented Windows.Identity

---
+++ RA.Utilities/Windows/Identity.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Principal;
+
+namespace RA.Utilities.Windows
+{
+    public static class Identity
+    {
+        public static string GetCurrentSID()
+        {
+            return WindowsIdentity.GetCurrent().User?.Value ?? string.Empty;
+        }
+    }
+}
