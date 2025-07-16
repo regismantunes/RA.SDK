d07afd1750946e5d80a655f0559331aabb4f1cb5
Regis Antunes <65835931+regismantunes@users.noreply.github.com>
2025-07-16 15:50:06 -0300

    New TaskExtensions

---
+++ RA.Utilities/Extensions/TaskExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace RA.Utilities.Extensions
+{
+    public static class TaskExtensions
+    {
+        public static async Task DelayOrCompleted(this Task task, int millisecondsDelay)
+        {
+            ArgumentNullException.ThrowIfNull(task);
+            ArgumentOutOfRangeException.ThrowIfLessThanOrEqual(millisecondsDelay, 0, nameof(millisecondsDelay));
+            
+            var delayTask = Task.Delay(millisecondsDelay);
+            await Task.WhenAny(task, delayTask);
+        }
+    }
+}
