adc0539d2c40ecbb000082eaa717d714277eea86
Regis Antunes <65835931+regismantunes@users.noreply.github.com>
2025-07-25 09:09:47 -0300

    Changed ActionExtensions.TryExecute to return bool

--- RA.Utilities/ErrorHandling/ActionExtensions.cs
+++ RA.Utilities/ErrorHandling/ActionExtensions.cs
@@ -4,15 +4,15 @@ namespace RA.Utilities.ErrorHandling
 {
     public static class ActionExtensions
     {
-        public static void TryExecute(this Action action, Action<Exception>? onError = null, params Type[] ignoreException)
+        public static bool TryExecute(this Action action, Action<Exception>? onError = null, params Type[] ignoreException)
         {
-            new CustomErrorHandling(onError, ignoreException)
+            return new CustomErrorHandling(onError, ignoreException)
                 .TryExecute(action);
         }
 
-        public static void TryExecute<T>(this Action action, T exceptionParameter, Action<T,Exception> onError, params Type[] ignoreException)
+        public static bool TryExecute<T>(this Action action, T exceptionParameter, Action<T,Exception> onError, params Type[] ignoreException)
         {
-            new CustomErrorHandling<T>(onError, ignoreException)
+            return new CustomErrorHandling<T>(onError, ignoreException)
                 .TryExecute(action, exceptionParameter);
         }
     }
