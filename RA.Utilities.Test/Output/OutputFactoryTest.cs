d9af69813ed4a4567f751cb6d89b00ccdae47d2a
Regis Antunes <65835931+regismantunes@users.noreply.github.com>
2025-08-11 08:41:56 -0400

    Implemented OutputFActory test

---
+++ RA.Utilities.Test/Output/OutputFactoryTest.cs
@@ -0,0 +1,19 @@
+﻿using RA.Utilities.Output;
+
+namespace RA.Utilities.Test.Output
+{
+    public class OutputFactoryTest
+    {
+        [Fact]
+        public void CreateOutput_WhenOutputTypeIsConsole_ShouldReturnOutputInstanceOfConsoleOutput()
+        {
+            // Arrange
+            var outputFactory = new OutputFactory();
+            // Act
+            var output = outputFactory.CreateOutput(OutputType.Console);
+            // Assert
+            Assert.NotNull(output);
+            Assert.IsType<ConsoleOutput>(output);
+        }
+    }
+}
