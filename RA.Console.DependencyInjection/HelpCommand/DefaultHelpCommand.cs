ee61eb829dfcbacb88a2f03730c862494d4e374f
Regis Antunes <65835931+regismantunes@users.noreply.github.com>
2025-10-29 17:09:51 -0400

    Improved DefaultHelpCommand to break description line when greater then console window

--- RA.Console.DependencyInjection/HelpCommand/DefaultHelpCommand.cs
+++ RA.Console.DependencyInjection/HelpCommand/DefaultHelpCommand.cs
@@ -17,7 +17,21 @@
 
                 foreach (var command in commandGroup.OrderBy(c => c.Order)
                                                     .ThenBy(c => c.Example))
-                    System.Console.WriteLine($"  {command.Example}{new string(' ', exampleMaxSize - command.Example.Length)} {command.Description}");
+                {
+                    var helpLineExample = $"  {command.Example}{new string(' ', exampleMaxSize - command.Example.Length)} ";
+                    var helpLineFull = string.Concat(helpLineExample, command.Description);
+                    if (helpLineExample.Length < System.Console.WindowWidth &&
+                        helpLineFull.Length > System.Console.WindowWidth)
+                    {
+                        var count = 1;
+                        do
+                        {
+                            helpLineFull = helpLineFull.Insert(System.Console.WindowWidth * count, string.Concat(Environment.NewLine, new string(' ', helpLineExample.Length)));
+                            count++;
+                        } while(helpLineFull.Length > System.Console.WindowWidth * count);
+                    }
+                    System.Console.WriteLine(helpLineFull);
+                }
             }
             
             return 0;
